e465bdbf061f05a75ea7d0d63d1277f9aa2517a8
Savannah-Lu <savannahlut@nycstudents.net>
2025-03-18 13:27:33 +0000

    trying.

--- APCalendar.java
+++ APCalendar.java
@@ -15,29 +15,31 @@ public class APCalendar{
     /** Returns the number of leap years between year1 and year2, inclusive.
     * Precondition: 0 <= year1 <= year2
     */
-    public static int numberOfLeapYears(int year1, int year2)
-    { /* to be implemented in part (a) */ }
+    public static int numberOfLeapYears(int year1, int year2){
+        int count = 0;
+        for(int i = year1; i <= year2; i++){
+            if(isLeapYear(i)){
+                count++;
+            }
+        }
+        return count;
+    }
 
     /** Returns the value representing the day of the week for the first day of year,
     * where 0 denotes Sunday, 1 denotes Monday, ..., and 6 denotes Saturday.
     */
-    private static int firstDayOfYear(int year)
-    { /* implementation not shown */ }
+    private static int firstDayOfYear(int year){}
 
     /** Returns n, where month, day, and year specify the nth day of the year.
     * Returns 1 for January 1 (month = 1, day = 1) of any year.
     * Precondition: The date represented by month, day, year is a valid date.
     */
-    private static int dayOfYear(int month, int day, int year)
-    { /* implementation not shown */ }
-
+    private static int dayOfYear(int month, int day, int year){}
     /** Returns the value representing the day of the week for the given date
     * (month, day, year), where 0 denotes Sunday, 1 denotes Monday, ...,
     * and 6 denotes Saturday.
     * Precondition: The date represented by month, day, year is a valid date.
     */
-    public static int dayOfWeek(int month, int day, int year)
-    { /* to be implemented in part (b) */ }
-
+    public static int dayOfWeek(int month, int day, int year){}
     // There may be instance variables, constructors, and other methods not shown.
 }
---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main{
+    public static void main(String[] args) {
+        
+        System.out.println(APCalendar.numberOfLeapYears(2020, 2024));
+    }
+}
