19aa26c6d9d8ffcdfdbfc9c1ca7ac46f3970d819
Savannah-Lu <savannahlut@nycstudents.net>
2025-03-20 13:23:19 +0000

    need to make non included methods

--- APCalendar.java
+++ APCalendar.java
@@ -35,11 +35,15 @@ public class APCalendar{
     * Precondition: The date represented by month, day, year is a valid date.
     */
     private static int dayOfYear(int month, int day, int year){}
+    
     /** Returns the value representing the day of the week for the given date
     * (month, day, year), where 0 denotes Sunday, 1 denotes Monday, ...,
     * and 6 denotes Saturday.
     * Precondition: The date represented by month, day, year is a valid date.
     */
-    public static int dayOfWeek(int month, int day, int year){}
+    public static int dayOfWeek(int month, int day, int year){
+        int additionalDays = dayOfYear(month, day, year) - 1;
+        return (firstDayOfYear(year) + additionalDays) % 7;
+    }
     // There may be instance variables, constructors, and other methods not shown.
 }
--- Main.java
+++ Main.java
@@ -2,5 +2,7 @@ public class Main{
     public static void main(String[] args) {
         
         System.out.println(APCalendar.numberOfLeapYears(2020, 2024));
+
+        
     }
 }
